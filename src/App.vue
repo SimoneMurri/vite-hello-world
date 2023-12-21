960b9572a17d9000342514e9429e221d076504f9
Simone Murri <simonemurri701@gmail.com>
2023-12-21 12:26:09 +0100

    Hello world vite e vue

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template lang="">
+  <div>
+    <AppTitle/>
+  </div>
+</template>
+
+
+<script>
+import AppTitle from './components/AppTitle.vue';
+export default {
+  components: {
+    AppTitle,
+  }
+}
+</script>
+
+
+<style lang="">
+  
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,15 @@
+<template lang="">
+    <div>
+        <h1>La mia prima app con vite!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="">
+    
+</style>
